b4ec6a5cebf458ed1913fefbe5327322055965ff
reymonera <marlen@Syd>
2025-02-03 14:09:10 -0500

    cutadapt finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# 16S-lima-wetlands
+# Pipeline for the 16S-metabarcoding characterization in Pantanos de Villa - Lima - Peru
+This computational pipeline is designed for comprehensive microbiome analysis of wetland samples using 16S rRNA gene sequencing. Developed to process complex environmental microbiome data, the pipeline provides a robust, reproducible workflow for taxonomic characterization and ecological insights in wetland microbial communities.
+
+## 03/02/2025 - Finished Cutadapt
+- Could not find a reverse complement primer in the evaluated sequences using the ´grep -iE´ command
+- Could find the 5'-3' oriented initial primers in all sequences.
+- Cutadapt was used for these primers only.
+- Any other treatment will have to be done using quality plot info from DADA2.
+
--- cutadapt_pipeline.bash
+++ cutadapt_pipeline.bash
@@ -5,9 +5,6 @@ source /home/marlen/miniforge3/etc/profile.d/conda.sh
 conda init
 conda activate dada2_env
 
-# Your commands here
-echo "Conda environment activated: $(conda info --envs)"
-
 # Function to display usage
 usage() {
    echo "Usage: $0 -b BARCODE_TSV -d INPUT_DIRECTORY"
@@ -36,7 +33,7 @@ done
 mkdir -p nonprim_reads
 
 # Process each sample
-while IFS=$'\t' read -r sampleid barcode primer rest; do
+while IFS=$'\t' read -r sampleid barcode primer barcodename revprimer projectname description; do
    # Skip header line
    [[ "$sampleid" == "#SampleID" ]] && continue
    
@@ -45,10 +42,14 @@ while IFS=$'\t' read -r sampleid barcode primer rest; do
    R2=$(find "$INPUT_DIRECTORY" -name "*${sampleid}*R2*.fastq*")
    
    # Skip if files not found
-   [[ -z "$R1" || -z "$R2" ]] && continue
+   # -g y -G se refieren a la orientación, y ambos son 5', así sea
+   # el forward o el reverse. No se encontraron reversos complementarios
+   # en el final de los reads, así que solo se quitó la primera
+   # secuencia que hacía match en los reads que se procedieron a quitar.
    
-   cutadapt -g "^$barcode" -G "^$barcode" \
-            -a "primer$" -A "primer$" \
+   [[ -z "$R1" || -z "$R2" ]] && continue
+   # -g "^$barcode" -G "^$barcode" \
+   cutadapt -g "^$primer" -G "^$revprimer" \
             -o "nonprim_reads/$(basename "$R1")" \
             -p "nonprim_reads/$(basename "$R2")" \
             "$R1" "$R2"
